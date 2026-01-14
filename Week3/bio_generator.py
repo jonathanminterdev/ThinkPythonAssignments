e0d97314b037fc0470d440c3c4c769172d5a1412
Jonathan Minter <jonathan@jonathanminter.dev>
2026-01-14 13:38:02 -0500

    STRUCTURE & PSEUDOCODE

---
+++ Week3/bio_generator.py
@@ -0,0 +1,10 @@
+# Import Statements
+
+# Function Definitions
+
+# Program Logic
+# Assign values to variables: name, goal, birth_year, current_year
+# Calculate Age
+# Print top border
+# Print bio sentence
+# Print bottom border
