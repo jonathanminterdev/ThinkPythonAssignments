d57cfd6c1214a70810fd8949714769450df92ef8
Jonathan Minter <jonathan@jonathanminter.dev>
2026-01-14 13:44:58 -0500

    MIDPOINT

--- Week3/bio_generator.py
+++ Week3/bio_generator.py
@@ -4,7 +4,14 @@
 
 # Program Logic
 # Assign values to variables: name, goal, birth_year, current_year
+name = "John Smith"
+goal = "I want to rule the world"
+birth_year = 1971
+current_year = 2026
 # Calculate Age
+age = current_year - birth_year
 # Print top border
+print("=" * 25)
 # Print bio sentence
+print("My name is " + name + " and I am " + str(age) + " years old.")
 # Print bottom border
